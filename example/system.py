79060a3489c825c52aed58c013d6ed2affc73321
SManAT <s.hagmann@bgbaden-frauen.ac.at>
2020-04-27 17:10:22 +0200

    Update system.py

--- example/system.py
+++ example/system.py
@@ -1,9 +1,7 @@
-import sys
-sys.exec_prefix
+#Add a Path to the System Path of Python, relative to your blender File
 import os
-rootPath = os.path.abspath(os.path.join(os.path.dirname(__file__)))
-libPath = os.path.join(rootPath, "test.py")
+rootPath = os.path.abspath(os.path.join(os.path.dirname(bpy.data.filepath)))
+libPath = os.path.join(rootPath, "")
 #add libPath to SystemPath
 sys.path.insert(0, libPath)
-
-print sys.path
+print(sys.path)
