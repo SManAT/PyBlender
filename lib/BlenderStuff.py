8c9903a5c124a24ac246d613caa0c0caa617ae90
SManAT <s.hagmann@bgbaden-frauen.ac.at>
2020-07-23 10:22:44 +0200

    Update BlenderStuff.py

--- lib/BlenderStuff.py
+++ lib/BlenderStuff.py
@@ -14,11 +14,41 @@ class BlenderStuff:
         self._myScene = bpy.context.scene
         self._C = bpy.context
         self._D = bpy.data
+        # creating uniq ID
+        self._globalCounter = 0
 
-    '''
-    Search and remove collection
-    '''
+    def selectObject(self, ob):
+        """Select via DataID"""
+        # Deselect all objects
+        bpy.ops.object.select_all(action='DESELECT')
+        # Make the cube the active object
+        self._C.view_layer.objects.active = ob
+        ob.select_set(True)
+        return ob
+
+    def selectObjectByName(self, name):
+        """Select via Name"""
+        # Get the object
+        ob = self._C.scene.objects[name]
+        return self.selectObject(ob)
+
+    def setToOrigin(self, ob):
+        """Move ob to 0,0,0"""
+        self.selectObject(ob)
+        ob.location = (0.0, 0.0, 0.0)
+
+    def duplicate(self, ob):
+        """makes a deep copy of the object"""
+        template_ob = self.selectObjectByName(ob)
+        if template_ob:
+            self._globalCounter += 1
+            newname = "%s-%s" % (template_ob.name, self._globalCounter)
+            ob = bpy.data.objects.new(newname, template_ob.data)
+            # ob = template_ob.copy() not a full copy
+            return ob
+
     def remove_Collection(self, col_name):
+        """Search and remove collection"""
         remove_collection_objects = True
         coll = bpy.data.collections.get(col_name)
         # found
@@ -29,10 +59,8 @@ class BlenderStuff:
                     bpy.data.objects.remove(obs.pop())
             bpy.data.collections.remove(coll)
 
-    '''
-    Create new collection and add it to scene
-    '''
     def create_Collection(self, col_name):
+        """Create new collection and add it to scene"""
         # first remove old ones
         self.remove_Collection(col_name)
         collection = self._D.collections.new(name=col_name)
@@ -40,10 +68,30 @@ class BlenderStuff:
         self._myScene.collection.children.link(collection)
         return collection
 
-    def add_to_Collection(self, col_name, objectdata):
+    def add_to_Collection(self, objectdata, col_name):
         """Add something to a collection."""
-        # add it to our specific collection
         self._D.collections[col_name].objects.link(objectdata)
+
+    def remove_from_Collection(self, objectdata, col_name):
+        """Remove something from a collection."""
+        self._D.collections[col_name].objects.unlink(objectdata)
+
+    def move_to_Collection(self, obj, col1, col2):
+        """Move Object from col1 to col2"""
+        # put the obj in the new collection
+        c1 = self._D.collections[col1]
+        c2 = self._D.collections[col2]
+        c2.objects.link(obj)
+        # remove it from the old collection
+        c1.objects.unlink(obj)
+
+    def get_obj_collection(name):
+        """determin collection in where the obj is"""
+        item = bpy.data.objects[name]
+        collections = item.users_collection
+        if len(collections) > 0:
+            return collections[0]
+        return bpy.contex.scene.collection
 
     def MakePolyLine(self, objname, curvename, cList, coll):
         """
