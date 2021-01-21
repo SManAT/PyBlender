0e6af5923ed78f02080a57b96ae87d2983f4e4b2
S.Hag <s.hagmann@bgbaden-frauen.ac.at>
2021-01-21 14:58:53 +0100

    Update Object.py

--- lib/Object.py
+++ lib/Object.py
@@ -1,4 +1,4 @@
-import bpy
+import bpy, math
 
 """
 Mag. Stefan Hagmann 2020
@@ -17,9 +17,47 @@ class Object:
 
         # creating uniq ID
         self._globalCounter = 0
+        
+    def translate(self, ob, deltax, deltay, deltaz):
+        """
+        Translate the object ob
+        :param deltax: x + deltax
+        :param deltax: y + deltay
+        :param deltax: z + deltaz
+        """
+        bpy.data.objects[ob.name].location.x += deltax
+        bpy.data.objects[ob.name].location.y += deltay
+        bpy.data.objects[ob.name].location.z += deltaz
+
+
+    def rotate(self, ob, alpha, axis):
+        """
+        Rotate the object ob
+        :param alpha: alpha in degrees
+        :param axis: the axis, x, y or z Global
+        """
+        # set the active object
+        self._C.view_layer.objects.active = ob
+        if axis.lower() == 'x':
+            bpy.data.objects[ob.name].rotation_euler[0] = math.radians(alpha)
+        if axis.lower() == 'y':
+            bpy.data.objects[ob.name].rotation_euler[1] = math.radians(alpha)
+        if axis.lower() == 'z':
+            bpy.data.objects[ob.name].rotation_euler[2] = math.radians(alpha)
+        
+
+    def scale(self, ob, fact):
+        """
+        Scale the object ob
+        :param fact: the scale factor 
+        """
+        bpy.data.objects[ob.name].scale = (fact, fact, fact)
+
 
     def selectObject(self, ob):
-        """Select via DataID"""
+        """
+        Select via DataID
+        """
         # Deselect all objects
         self.deselectAll()
         # Make the cube the active object
